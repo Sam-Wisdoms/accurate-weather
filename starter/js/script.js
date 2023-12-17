9de8524d0a6bc5bb670fc0663ba18d87658ecb35
Sam-Wisdoms Amenyenu <sampson.aglanu@gmail.com>
2023-12-17 22:06:00 +0000

    Added Some JS

--- starter/js/script.js
+++ starter/js/script.js
@@ -1,25 +1,64 @@
-console.log('test')
+const historyElement = document.getElementById('history')
+const todayElement = document.getElementById('today')
+const forecastElement = document.getElementById('forecast')
+const searchFormElement = document.getElementById('search-form')
+const searchInputElement = document.getElementById('search-input') 
+const currentWeather = document.querySelector('.current-Weather')
+
+
+
+// https://openweathermap.org/img/w/' + weather[0].icon + '.png
+
+// //for example
+// https://openweathermap.org/img/w/04n.png
 
 
 // fetch('https://api.openweathermap.org/data/2.5/forecast?q=london&+&appid=326cdcca5d5f8ca6a617c5ff3750800a')
 
-$('#search-form').on('click', function(event) {
+
+// link for icon
+https://openweathermap.org/img/w/' + weather[0].icon + '.png
+
+
+
+
+searchFormElement.addEventListener('submit', function(event) {
     event.preventDefault()
-    // console.log(event.target)
-    let cityName = $('#search-input').val()
-    console.log(cityName)
-    getDataFromApi(cityName)
+  
+    let cityName = searchInputElement.value
+   
+    getDataFromApi(cityName) // Passing city name to this function.
 })
 
 function getDataFromApi(cityName) {
     // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=326cdcca5d5f8ca6a617c5ff3750800a')
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=326cdcca5d5f8ca6a617c5ff3750800a&units=metric')
 .then(function(response) {
     return response.json()
     // console.log(response)
 })
 .then(function(data) {
-    console.log(data)
+    console.log(data);
+    // Date, time, tempretature, wind speed, humidity, weather icon
+    let currentWeatherHTML = `` 
+
+    console.log(data.list[0].dt_txt)
+
+
+    const markup = `<div>
+    <h2>${data.city.name.toUpperCase()} ${data.list[0].dt_txt}</h2>
+    <p>Temp: ${data.list[0].main.temp}</p>
+    <p> Wind: ${data.list[0].wind.speed}</p>
+    <p> Humidity: ${data.list[0].main.humidity}</p>
+    
+    </div>` 
+
+    currentWeather.innerHTML = markup
+
+
+
+
+  
 })
 }
 
