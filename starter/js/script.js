c83dbbd8cd413aadc27b63b01665b8e892fdda3d
Sam-Wisdoms Amenyenu <sampson.aglanu@gmail.com>
2023-12-19 02:29:52 +0000

    Added individual card conditions and extrated data

--- starter/js/script.js
+++ starter/js/script.js
@@ -43,9 +43,9 @@ function getDataFromApi(cityName) {
         const currentWeatherHTML = `
         <div class= "card-body">
             <h3 id="location-name" class="card-title">${data.city.name} ${dateOnly}</h3>
-            <p id="current-temp" class="card-text">Temperature: <span>${data.list[0].main.temp}</span></p>
-            <p id="current-wind" class="card-text">Wind: <span>${data.list[0].wind.speed}</span></p>
-            <p id="current-humidity" class="card-text">Humidity: <span>${data.list[0].main.humidity}</span></p>
+            <p id="current-temp" class="card-text">Temperature: <span>${data.list[0].main.temp}°C</span></p>
+            <p id="current-wind" class="card-text">Wind: <span>${data.list[0].wind.speed} Km/hr</span></p>
+            <p id="current-humidity" class="card-text">Humidity: <span>${data.list[0].main.humidity} %</span></p>
         </div>
         `
 
@@ -58,6 +58,42 @@ function getDataFromApi(cityName) {
             let forecastItem = data.list[i];
             const dateAndTime = new Date(forecastItem.dt_txt);
             const dateOnly = dateAndTime.toISOString().split('T')[0]
+
+            const singleForecastHTML = `
+            <div class="card single-forecast">
+                <div class="card-body">
+                    <h5 class="card-title forecast-date">${dateOnly}</h5>
+                    <h5 class="card-title forecast-date">Image</h5>
+                    <!-- <img src="" alt="weather conditions"-->
+                    <p class="card-text forecast-temp">Temp: ${forecastItem.main.temp} °C</p>
+                    <p class="card-text forecast-wind">Wind: ${forecastItem.wind.speed} Km/hr</p>
+                    <p class="card-text forecast-humidity">Humidity: ${forecastItem.main.humidity} %</p>
+                </div>
+            </div>
+            `
+            weatherForecastHTML += singleForecastHTML
+        }
+
+        forecastContainerEl.innerHTML = weatherForecastHTML
+
+        let weatherData = []
+
+        for(let i = 0; i < data.list.length; i+8) {
+            const dateData = data.list[i].dt_txt;
+            const dateAndTime = new Date(dateData);
+            const dateOnly = dateAndTime.toISOString().split('T')[0]
+
+            const temp = data.list[i].main.temp
+            const wind = data.list[i].wind.speed
+            const humidity = data.list[i].main.humidity
+
+            const extractedWeatherData ={
+                dateData: dateOnly,
+                tempData: temp,
+                windData: wind,
+                humidityData: humidity,
+            }
+            weatherData.push(extractedWeatherData)
         }
     })
 }
