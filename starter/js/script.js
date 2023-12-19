3fe6d524d6a88f63c12dc7d44bcc9a1c66a4f7fc
Sam-Wisdoms Amenyenu <sampson.aglanu@gmail.com>
2023-12-19 01:34:38 +0000

    Fetched Dat, Coditions, Looped

--- starter/js/script.js
+++ starter/js/script.js
@@ -1,123 +1,64 @@
-const historyElement = document.getElementById('history')
-const todayElement = document.getElementById('today')
-const forecastElement = document.getElementById('forecast')
-const searchFormElement = document.getElementById('search-form')
-const searchInputElement = document.getElementById('search-input') 
-const currentWeather = document.querySelector('.current-Weather')
+// Targeting various HTML element classes and IDs
+const searchInputEl = document.getElementById('search-input');
+const searchFormEl = document.getElementById('search-form');
+const forecastContainerEl = document.querySelector('all-forecast-cards');
+const searchHistoryEl = document.querySelector('.search-history');
+const currentWeatherEL = document.querySelector('.current-weather');
+const forecastTitleEl = document.querySelector('.forecast-title');
 
-const mainCardHeader = document.querySelector('.main-header')
-const cityTitle = document.querySelector('.city-title')
-const tempText = document.querySelector('.temp-text')
-const humidityText = document.querySelector('.humidity-text')
-const windText = document.querySelector('.wind-text')
-
-// https://openweathermap.org/img/w/' + weather[0].icon + '.png
-
-
-
-searchFormElement.addEventListener('submit', function(event) {
+//Add eventlistener to search form
+searchFormEl.addEventListener('submit', function(event) {
     event.preventDefault()
-  
-    let cityName = searchInputElement.value
-   
-    getDataFromApi(cityName) // Passing city name to this function.
-})
 
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+    let cityName = searchInputEl.value; 
+    if(cityName !== "") {
+        getDataFromApi(cityName)
+        forecastTitleEl.classList.remove('hide')
+        searchInputEl.value = ""
+
+        const newHistoryBtn = document.createElement('button')
+        newHistoryBtn.textContent = cityName.trim()
+        newHistoryBtn.classList.add('history-btn')
+
+        if(searchHistoryEl.children.length > 0) {
+            searchHistoryEl.insertBefore(newHistoryBtn, null)
+        } else {
+            searchHistoryEl.appendChild(newHistoryBtn)
+        }
+    }       
+})
 
 function getDataFromApi(cityName) {
-    // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=326cdcca5d5f8ca6a617c5ff3750800a&units=metric')
-.then(function(response) {
-    return response.json()
-    // console.log(response)
-})
-.then(function(data) {
-    console.log(data);
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + 
+    '&appid=326cdcca5d5f8ca6a617c5ff3750800a&units=metric')
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        // console.log(data)
+        const dateAndTime = new Date(data.list[0].dt_txt);
+        const dateOnly = dateAndTime.toISOString().split('T')[0];
 
-  
-    // Date, time, tempretature, wind speed, humidity, weather icon
-    let currentWeatherHTML = `` 
+        // Remember to search and add units
+        const currentWeatherHTML = `
+        <div class= "card-body">
+            <h3 id="location-name" class="card-title">${data.city.name} ${dateOnly}</h3>
+            <p id="current-temp" class="card-text">Temperature: <span>${data.list[0].main.temp}</span></p>
+            <p id="current-wind" class="card-text">Wind: <span>${data.list[0].wind.speed}</span></p>
+            <p id="current-humidity" class="card-text">Humidity: <span>${data.list[0].main.humidity}</span></p>
+        </div>
+        `
 
-    // console.log(data.list[0].dt_txt)
-    // const markup = `<div>
-    // <h2>${data.city.name.toUpperCase()} ${data.list[0].dt_txt}</h2>
-    // <p>Temp: ${data.list[0].main.temp}</p>
-    // <p> Wind: ${data.list[0].wind.speed}</p>
-    // <p> Humidity: ${data.list[0].main.humidity}</p>
-    // </div>` 
+        currentWeatherEL.innerHTML = currentWeatherHTML
 
-    // currentWeather.innerHTML = markup
+        let weatherForecastHTML = ""
 
- cityTitle.textContent = `City: ${data.city.name.toUpperCase()} ${data.list[0].dt_txt}`
- tempText.textContent = `Temperature: ${data.list[0].main.temp}`
- humidityText.textContent = `Humidity: ${data.list[0].main.humidity}`
- windText.textContent = `Wind: ${data.list[0].wind.speed}`
-    
-
-
-  
-})
+        // Write for loop that can loop through all items
+        for(let i = 0; i < data.list.length; i = i + 8) {
+            let forecastItem = data.list[i];
+            const dateAndTime = new Date(forecastItem.dt_txt);
+            const dateOnly = dateAndTime.toISOString().split('T')[0]
+        }
+    })
 }
 
-// Function to render data for 5 day forecast
-async function renderData() {
-    const container = document.querySelector('.container');
-    const data = await fectchData();
-
-    if(!data){
-        return;
-    }
-
-    data.forEach(item => {
-        const card = document.createElement('div');
-        card.classList.add('card');
-
-        const title = document.createElement('h2');
-        title.textContent = item.title;
-
-        const body = document.createElement('p');
-        body.textContent = item.body;
-        
-    });
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function fiveDayForecast(lat, lon) {
-//     var URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=326cdcca5d5f8ca6a617c5ff3750800a`;
-//     fetch(URL)
-//     .then(res => {
-//         return res.json()
-//     })
-//     .then(data => {
-//         console.log("DATA: ", data)
-//     })
-// }
-
-// https://openweathermap.org/forecast5#name5
-
-// Built in API request by city name
-
-// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-
-// api.openweathermap.org/data/2.5/forecast?q={city name},{country code}&appid={API key}
-
-//
